6758cf0a608175bae93a642b9f82bd45d6ba4585
walimi <wahid.alimi@gmail.com>
2016-07-21 17:53:17 -0400

    Initial checkin of multimath with some code. this code doesn't work yet.

--- Javascript_Modules/multimath/js/app.js
+++ Javascript_Modules/multimath/js/app.js
@@ -1,5 +1,19 @@
 // Basic structure of an immediatly IIFE (Immediately Invoked Function Expression)
 (function () {
-
+    console.log("Starting Multimath with an IIFE");
+    
+    // add click handler to the start game button
+    document.getElementById("startGame").addEventListener("click", function() {
+        player.setName(document.getElementById("playername").value);
+        game.printGame();
+    });
+    
+    // add click handler to the calculate score button
+    document.getElementById("calculte").addEventListener("click", function() {
+        game.calculateScore();
+    });
+    
+    // set the default number of problems
+    document.getElementById("problemCount").value = game.getProblemCount();
     
 })();
