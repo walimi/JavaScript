9ec217b4ea5b5edd3bcd5fddd18edd338ce6b467
Wahid Alimi <WahidAlimi@deltek.com>
2016-10-14 18:56:34 -0400

    Using promises.

--- JavaScript_Promises/JavaScript_Promises.js
+++ JavaScript_Promises/JavaScript_Promises.js
@@ -1,26 +1,56 @@
-// Using callbacks
+// Using Promises
 
-var teslaImageUrl = "https://tctechcrunch2011.files.wordpress.com/2015/08/tesla_model_s.jpg";
-loadImage(teslaImageUrl, 
-    function onsuccess(img) {
-        // Add the image to the page
-        document.body.appendChild(img);
-    },    
-    function onerror(e) {
-        console.log("Error occurred while loading image");
-        console.log(e);
-    });
+var displayImage1 = {
+  show: function(url) {
+    car.getCarImage(url).then(function(img) {
+      document.body.appendChild(img);
+    }).catch(function(e) {
+      console.log("There was an error in displayImage1.");
+    }); 
+  }
+}; 
 
 
-function loadImage(url, success, error) {
-    var img = new Image();
-    img.src = url;
+var displayImage2 = {
+  show: function(url) {
+    car.getCarImage(url).then(function(img) {
+      console.log("Image found");
+    }).catch(function(e) {
+      console.log("There was an error in displayImage2.");
+    }); 
+  }
+};
 
-    img.onload = function () {
-        success(img);
-    };
+var self = this;
 
-    img.onerror = function (e) {
-        error(e);
-    };
+var car = {
+  carImagePromise: null, 
+  getCarImage: function(url) {
+    if(!this.carImagePromise) {
+      this.carImagePromise = self.loadImage(url);
+    }
+    return this.carImagePromise;    
+  }  
+};
+
+function loadImage(url) {
+  var promise = new Promise(
+    function resolver(resolve, reject) {
+      var img = new Image();
+      img.src = url;
+      
+      img.onload = function() {
+        resolve(img);
+      }
+      
+      img.onerror = function(e) {
+        reject(e);
+      }
+    }
+  );
+  return promise;
 }
+
+var url = "https://tctechcrunch2011.files.wordpress.com/2015/08/tesla_model_s.jpg";
+displayImage1.show(url);
+displayImage2.show(url);
